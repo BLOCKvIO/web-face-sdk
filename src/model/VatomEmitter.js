427191b87df70fa444a55061173c562487f05542
LordCheddar <lordcheddar@gmail.com>
2019-03-01 16:46:05 +0200

    add observe children

--- src/model/VatomEmitter.js
+++ src/model/VatomEmitter.js
@@ -2,8 +2,9 @@ import Vatom from './Vatom';
 import Emitter from '../util/EventEmitter';
 
 export default class VatomEmitter extends Vatom {
-  constructor(data) {
+  constructor(data, bridge) {
     super(data);
+    this.bridge = bridge;
     this.emitter = new Emitter();
   }
 
@@ -16,6 +17,11 @@ export default class VatomEmitter extends Vatom {
 
   addEventListener(name, callback) {
     this.emitter.on(name, callback);
+    if (name === 'children') {
+      this.bridge.sendMessage('core.vatom.children.observe', { id: this.id })
+        .then(response => response.vatoms.map(vatom => new Vatom(vatom)))
+        .then(vatoms => this.emitter.emit('children', vatoms));
+    }
   }
 
   removeEventListener(name, callback) {
